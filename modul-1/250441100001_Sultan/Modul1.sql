6ad33f91d6878301786b273f9de3db088a93fa5d
sultan-16-bit <sultanyuni05@gmail.com>
2026-06-19 08:36:57 +0700

    250441100001_SULTAN_modul1_KakRaka

---
+++ modul-1/250441100001_Sultan/Modul1.sql
@@ -0,0 +1,23 @@
+-- MODUL1--
+CREATE TABLE koleksi_langka (
+id_buku BIGINT PRIMARY KEY,
+judul VARCHAR (250),
+tahun_terbit YEAR,
+estimasi_harga FLOAT
+)
+
+CREATE TABLE pelanggan_setia(
+nomor_identitas INT
+);
+ALTER TABLE pelanggan_setia
+MODIFY nomor_identitas VARCHAR(20)
+
+CREATE TABLE transaksi_harian(
+barang VARCHAR (200),
+harga BIGINT
+)
+
+CREATE TABLE umpan_balik (
+    nama VARCHAR (200),
+    isi_komentar TEXT
+)
